a7c6afa473103b7a688eb9bb15efe5979787a5ab
KimSungKwon <sungkwonn@naver.com>
2020-11-21 15:18:39 +0900

    Updates

---
+++ precache-manifest.36019572cf18e9b0951678e9772e8ecf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f38e8f8de98a4c76c78e6f53e8c735c",
+    "url": "/street_vender/index.html"
+  },
+  {
+    "revision": "07b78f97718ab8bb978e",
+    "url": "/street_vender/static/css/2.2846713d.chunk.css"
+  },
+  {
+    "revision": "91dd769a4a00c1127b0a",
+    "url": "/street_vender/static/css/main.5107d2d7.chunk.css"
+  },
+  {
+    "revision": "07b78f97718ab8bb978e",
+    "url": "/street_vender/static/js/2.d0bb77a2.chunk.js"
+  },
+  {
+    "revision": "92d895d65c2c2711df9a109b93320b02",
+    "url": "/street_vender/static/js/2.d0bb77a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91dd769a4a00c1127b0a",
+    "url": "/street_vender/static/js/main.c3c2b2df.chunk.js"
+  },
+  {
+    "revision": "2831dbc2006916eb8f3a",
+    "url": "/street_vender/static/js/runtime-main.0fd09a0b.js"
+  }
+]);
