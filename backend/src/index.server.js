640a0f92d7b618b7952dcb8af4407f9474ad7e62
Sejun Park <qkrtpwns1999@naver.com>
2020-11-24 14:34:39 +0900

    dir 구조 변경

--- backend/src/index.server.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDomServer from 'react-dom/server';
-
-const html = ReactDomServer.renderToString(
-    <div>Hello Server Side Rendering</div>
-);
-
-console.log(html);
