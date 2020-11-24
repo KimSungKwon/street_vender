beaaf90886562b6d63c2b1a9da6825f3cc3077f6
Sejun Park <qkrtpwns1999@naver.com>
2020-11-24 14:04:09 +0900

    commit build

---
+++ frontend/build/precache-manifest.46fc069ec118e12e46242ea19bd60963.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8241ff4faeefcab2c78c7dbd16618e39",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e1506319fd36bacb7e6",
+    "url": "/static/css/2.2846713d.chunk.css"
+  },
+  {
+    "revision": "97b698c07ee69e5d072b",
+    "url": "/static/css/main.8d7bb460.chunk.css"
+  },
+  {
+    "revision": "9e1506319fd36bacb7e6",
+    "url": "/static/js/2.fb0d36c3.chunk.js"
+  },
+  {
+    "revision": "efb76f21dfa50a23107eb5dac6610283",
+    "url": "/static/js/2.fb0d36c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97b698c07ee69e5d072b",
+    "url": "/static/js/main.fecb2eba.chunk.js"
+  },
+  {
+    "revision": "38a0a9922dec8eed2f00",
+    "url": "/static/js/runtime-main.e7261440.js"
+  }
+]);
