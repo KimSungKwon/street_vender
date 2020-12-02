56e182ce9ef43ceaf6f91393c84968c8768bcb0b
Sejun Park <qkrtpwns1999@naver.com>
2020-12-02 22:03:00 +0900

    pagination 삭제

--- frontend/src/pages/PostListPage.js
+++ frontend/src/pages/PostListPage.js
@@ -10,7 +10,7 @@ const PostListPage = () => {
             
             <GoogleMapAPIContainer />
             <PostListContainer />
-            <PaginationContainer/>
+            {/*<PaginationContainer/>*/}
             <HeaderContainer />
         </>
     )
