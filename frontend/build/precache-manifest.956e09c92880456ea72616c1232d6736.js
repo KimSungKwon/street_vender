7ab016b5ea5426e9e4de39788409574a2add30e8
Sejun Park <qkrtpwns1999@naver.com>
2020-12-06 16:08:59 +0900

    build 추가

--- frontend/build/precache-manifest.956e09c92880456ea72616c1232d6736.js
+++ frontend/build/precache-manifest.956e09c92880456ea72616c1232d6736.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8241ff4faeefcab2c78c7dbd16618e39",
+    "revision": "362fa67eed4fcf2f84383323a963e3ff",
     "url": "/index.html"
   },
   {
-    "revision": "9e1506319fd36bacb7e6",
+    "revision": "e236325a5b5025ef7deb",
     "url": "/static/css/2.2846713d.chunk.css"
   },
   {
-    "revision": "97b698c07ee69e5d072b",
+    "revision": "a25b3fb253c96ec413a5",
     "url": "/static/css/main.8d7bb460.chunk.css"
   },
   {
-    "revision": "9e1506319fd36bacb7e6",
-    "url": "/static/js/2.fb0d36c3.chunk.js"
+    "revision": "e236325a5b5025ef7deb",
+    "url": "/static/js/2.8bbe3fd9.chunk.js"
   },
   {
     "revision": "efb76f21dfa50a23107eb5dac6610283",
-    "url": "/static/js/2.fb0d36c3.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.8bbe3fd9.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "97b698c07ee69e5d072b",
-    "url": "/static/js/main.fecb2eba.chunk.js"
+    "revision": "a25b3fb253c96ec413a5",
+    "url": "/static/js/main.e0195cac.chunk.js"
   },
   {
     "revision": "38a0a9922dec8eed2f00",
